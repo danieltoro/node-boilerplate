d22ffe2b5b3ac4ec9b52e861e3effc587afbe9ed
danieltoro <d.toro@outlook.com>
2018-10-31 00:17:09 -0300

    Added express middlewares

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -2,9 +2,12 @@
 * NPM Depedencies
 *
 * */
+const express = require('express');
 const morgan = require('morgan');
 
 module.exports = app => {
+    app.use(express.json());
+    app.use(express.urlencoded({extended: true}));
     app.use(morgan('dev'))
 };
 
